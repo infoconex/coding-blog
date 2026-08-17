9979b5a2eb98da26164805dbe0edb2caae8f09a4
Jim Scott <jscott@infoconex.com>
2026-08-16 22:00:16 -0700

    Remove abandoned Astro configuration

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://infoconex.github.io',
-  base: '/coding-blog',
-  output: 'static',
-  trailingSlash: 'never',
-});
