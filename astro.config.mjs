a6d59e84bc8e39eabb6f92a7b40fb6170e3307bf
Jim Scott <jscott@infoconex.com>
2026-08-16 21:56:39 -0700

    Configure Astro for GitHub Pages in issue #1

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://infoconex.github.io',
+  base: '/coding-blog',
+  output: 'static',
+  trailingSlash: 'never',
+});
